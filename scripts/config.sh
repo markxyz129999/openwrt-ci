1180cf85a8db3e6730f20786ae738abfc139ba41
markxyz129999 <markxyz129999@gmail.com>
2022-11-19 15:42:19 +0800

    Remove and add plugins

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.0.254/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+
+# Disable ntp server
+sed -i "s/set system.ntp.enable_server='1'/set system.ntp.enable_server='0'/g" package/base-files/files/bin/config_generate
